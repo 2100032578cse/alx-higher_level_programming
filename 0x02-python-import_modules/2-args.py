9a28f4a65c0c5421487b8913c85e297461513bcb
2100032578cse <uwizeyesandri@gmail.com>
2023-11-30 04:00:27 -0800

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from sys import argv
+i = 1
 if __name__ == "__main__":
     leng = len(argv)
     if leng == 1:
@@ -9,7 +10,6 @@ if __name__ == "__main__":
         print("1: {}".format(argv[1]))
     else:
         print('{} arguments:'.format(leng - 1))
-        i = 1
         while i < leng:
             print("{:d}{:s}".format(i, argv[i]))
             i = i + 1
