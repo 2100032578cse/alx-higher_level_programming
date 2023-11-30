f4ca1dcbdd418d6c29ac0bd44ba75c0b69fcb3fe
2100032578cse <uwizeyesandri@gmail.com>
2023-11-30 05:48:53 -0800

    maggy

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,25 +2,21 @@
 from sys import argv
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    leng = len(argv) - 1
-    if leng != 3:
+    length = len(argv) - 1
+    if (length != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-        ops = argv[2]
-        if not (ops == '+' or ops == '-' or ops == '*' or ops == '/'):
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-        a = int(argv[1])
-        b = int(argv[3])
-        addition = add(a, b)
-        mult = mul(a, b)
-        subt = sub(a, b)
-        divs = div(a, b)
-        if ops == '+':
-            print("{} + {} = {}".format(a, b, addition))
-        if ops == '-':
-            print("{} - {} = {}".format(a, b, subt))
-        if ops == '*':
-            print("{} * {} = {}".format(a, b, mult))
-        if ops == '/':
-            print("{} / {} = {}".format(a, b, divs))
+    ops = argv[2]
+    if not (ops == '+' or ops == '-' or ops == '*' or ops == '/'):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    if (ops == '+'):
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif (ops == '-'):
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif (ops == '*'):
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))
