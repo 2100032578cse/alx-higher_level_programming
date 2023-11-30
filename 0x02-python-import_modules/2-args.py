54c78140a60d58e3c66d0c4d102cac98e303ec91
2100032578cse <uwizeyesandri@gmail.com>
2023-11-30 06:04:08 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-   leng = len(argv) - 1
-      if leng == 0:
+    leng = len(argv) - 1
+    if leng == 0:
         print("{} arguments.".format(leng))
-      else:
-          if leng == 1:
-              print("{} argument:".format(leng))
-          else:
-              print("{} arguments:".format(leng))
+    else:
+        if leng == 1:
+            print("{} argument:".format(leng))
+        else:
+            print("{} arguments:".format(leng))
         i = 1
-        while(i <= leng):
+        while (i <= leng):
             print("{}: {}".format(i, argv[i]))
             i += 1
