7f63e947594f2b56e723fb4b7e89438ac96cb97f
2100032578cse <uwizeyesandri@gmail.com>
2023-12-04 08:57:53 -0800

    fixing file

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
     tup = ()
     n = len(sentence)
     if n != 0:
-        tup = len, sentence[0]
+        tup = n, sentence[0]
     else:
         tup = 0, "None"
     return tup
