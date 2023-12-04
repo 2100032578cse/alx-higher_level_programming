a784413614c083d7a007f02169ecec7ea2b131a4
2100032578cse <uwizeyesandri@gmail.com>
2023-12-04 10:21:36 -0800

    added revlist.c inside

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,25 @@
 #include "lists.h"
 /**
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
+ *
+ * Return: pointer to the first node in the new list
+ */
+void reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL, *next = NULL, *current = *head;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+}
+/**
  * is_palindrome - checks palindrome list
  * @head: the head of list
  * Return: an integer 0 or 1
