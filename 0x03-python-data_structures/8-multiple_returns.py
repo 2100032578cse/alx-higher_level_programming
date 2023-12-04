cbaae870dc5616b7b18232e358502fe274ce1aa3
2100032578cse <uwizeyesandri@gmail.com>
2023-12-04 08:56:03 -0800

    mult return

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    tup = ()
+    n = len(sentence)
+    if n != 0:
+        tup = len, sentence[0]
+    else:
+        tup = 0, "None"
+    return tup
