51c07527a3717af1cf2b347f2abf2ce606acc05b
2100032578cse <uwizeyesandri@gmail.com>
2023-12-06 06:04:16 -0800

    mult by 2 elements

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     dictnew = a_dictionary.copy()
     for i in a_dictionary.keys():
-        dictnew[i]= a_dictionary[i] * 2
+        dictnew[i] = a_dictionary[i] * 2
     return dictnew
