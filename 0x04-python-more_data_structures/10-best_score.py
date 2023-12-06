ea42f8aeb2348c7927b1ace581dbb4b299112b97
2100032578cse <uwizeyesandri@gmail.com>
2023-12-06 06:19:48 -0800

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        ans = max(a_dictionary, key = a_dictionary.get)
+        ans = max(a_dictionary, key=a_dictionary.get)
         return ans
     return None
