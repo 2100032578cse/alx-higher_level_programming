8e3fbc31ace936337aae3bb7bf9188025d5f98e7
2100032578cse <uwizeyesandri@gmail.com>
2023-12-06 08:23:01 -0800

    pythonc

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<Python.h>
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
+/**
+ * print_python_bytes - calculate python bytes
+ * @p: given pyh
+ * Return: nothing
+*/
+void print_python_bytes(PyObject *p)
+{
+
+}
+/**
+ * print_python_list - calculate python list
+ * @p: given pyh
+ * Return: nothing
+*/
+void print_python_list(PyObject *p)
+{
+
+}
