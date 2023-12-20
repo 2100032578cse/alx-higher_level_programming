a3aad80ba340e57dcb789f5041f3031445292c0d
2100032578cse <uwizeyesandri@gmail.com>
2023-12-20 09:23:14 -0800

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        elif:
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+        size.__size = size
+    def __area(self):
+        return size.__size ** 2
