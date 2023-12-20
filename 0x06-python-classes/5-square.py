54ae44f412ad9ec7ee861c5807dae740d53c6657
2100032578cse <uwizeyesandri@gmail.com>
2023-12-20 10:00:56 -0800

    taskt5 fix

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -36,6 +36,6 @@ class Square:
             [print("#", end="") for j in range(self.__size)]
             print("")
         if self.__size == 0:
-            print()
+            print("")
     
     
