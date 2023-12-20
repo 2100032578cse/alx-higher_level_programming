bd222e029cccfdf2cd9a26a2c3336d70de26e054
2100032578cse <uwizeyesandri@gmail.com>
2023-12-20 09:44:07 -0800

    fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+"""define a class"""
+
 class Square:
+    """pass nothing"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""define a class"""
 class Square:
+    """the init function ans main"""
     def __init__(self, size):
+        """
+        Initializes square
+        Attributes:
+            size: size of a square
+        """
         self.__size = size
