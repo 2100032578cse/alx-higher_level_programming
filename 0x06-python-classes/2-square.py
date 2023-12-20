fee695648de3603a2f092532e99cc40c30eb47a5
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2023-12-20 23:15:53 +0530

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/python3#!/usr/bin/python3
+"""Defines a class Square."""
+
+
 class Square:
+    """Represents a square."""
+
     def __init__(self, size=0):
-        if size < 0:
+        """Initialize a new Square.
+
+        Args:
+            size (int): square size.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        elif:
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-        size.__size = size
+        self.__size = size
