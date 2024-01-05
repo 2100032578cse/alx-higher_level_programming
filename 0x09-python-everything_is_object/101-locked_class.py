316d3e1b57f010a2c8b706b976e7e52280232727
2100032578cse <uwizeyesandri@gmail.com>
2024-01-05 07:05:46 -0800

    fixing

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,6 +6,8 @@ class LockedClass:
     """ user will not be able to create anew class"""
 
     __slot__ = ["first_name"]
+
 def __init__(self):
     """ for creating it"""
+
     self.first_name = "first_name"
