ef12aceac2a5bb7b328dcc77ba1d50a54901557b
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-01-05 20:34:49 +0530

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ locked class task"""
 
+
 class LockedClass:
     """ user will not be able to create anew class"""
 
     __slot__ = ["first_name"]
+def __init__(self):
+    """ for creating it"""
+    self.first_name = "first_name"
