8a231059681ecd805aa1da3f0e5a499f3b25b943
2100032578cse <uwizeyesandri@gmail.com>
2024-01-09 08:34:49 -0800

    task 10 fix

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -18,8 +18,8 @@ class Student:
         """retrieves a dictionary representation
         of a Student instance
         """
-        if attr is not None:
-            res = {k: self.__dict__[k] for k in self.__dict__.keys() & attr}
+        if attrs is not None:
+            res = {k: self.__dict__[k] for k in self.__dict__.keys() & attrs}
             return res
         else:
             return self.__dict__
