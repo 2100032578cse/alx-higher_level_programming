218cbcc959d1eacb150aa52f31b62fb564dcb8bc
2100032578cse <uwizeyesandri@gmail.com>
2024-01-09 08:24:13 -0800

    now

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -22,7 +22,8 @@ class Student:
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
-        def reload_from_json(self, json):
+
+    def reload_from_json(self, json):
         """
             replaces all attributes of the Student Instance.
             Args:
