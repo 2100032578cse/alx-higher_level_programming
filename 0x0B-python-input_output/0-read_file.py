3c926f014a761d9d45b9e8d4b52a9b66aade9482
2100032578cse <uwizeyesandri@gmail.com>
2024-01-09 06:58:38 -0800

    file permissions

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""func for reading a file"""
+
+
 def read_file(filename=""):
+    """ read file function is used"""
+
     with open(filename, 'r', encoding = 'utf-8') as fl:
         for a in fl:
             print(a, end="")
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" for write file"""
+
+
 def write_file(filename="", text=""):
+    """ we use write and 'w' for writing """
+
     with open(filename, 'w', encoding = 'utf-8') as fl:
         return fl.write(text)
