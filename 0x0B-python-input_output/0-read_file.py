daaac6f4c1315118fd42dfd5776e963243781ee7
2100032578cse <uwizeyesandri@gmail.com>
2024-01-09 07:00:52 -0800

    fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,7 @@
 def read_file(filename=""):
     """ read file function is used"""
 
-    with open(filename, 'r', encoding = 'utf-8') as fl:
+    with open(filename, 'r', encoding='utf-8') as fl:
         for a in fl:
             print(a, end="")
     fl.close()
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """ we use write and 'w' for writing """
 
-    with open(filename, 'w', encoding = 'utf-8') as fl:
+    with open(filename, 'w', encoding='utf-8') as fl:
         return fl.write(text)
