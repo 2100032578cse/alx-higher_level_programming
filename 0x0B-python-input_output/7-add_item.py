189d36957bcd79feb2dd4bbe094b31b2e5af963c
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-01-09 21:20:03 +0530

    task 7 fixing

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,20 +2,18 @@
 """ func to add arguments to python list"""
 
 
-import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+from sys import argv
 
 
 if __name__ == "__main__":
-
-    filename = "add_item.json"
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        lst = load_from_json_file(filename)
+        items = load_from_json_file('add_item.json')
     except FileNotFoundError:
-        lst = []
-
-    for arg in sys.argv[1:]:
-        lst.append(arg)
-    save_to_json_file(lst, filename)
+        items = []
+    argv.pop(0)
+    items.extend(argv)
+    save_to_json_file(items, 'add_item.json')
