d76ab78e25260942bc11f0a67ac9fa9b179ae721
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-01-10 08:06:56 +0530

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,29 +4,7 @@ and then save them to file:
 """
 
 
-import sys
-from os import path
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
-
-
-def add_item():
-    """Check if the file 'add_item.json' exists"""
-
-    if path.exists('add_item.json'):
-        my_list = load_from_json_file('add_item.json')
-    else:
-        my_list = []
-
-    my_list.extend(sys.argv[1:])
-
-    save_to_json_file(my_list, 'add_item.json')
-
-
-if __name__ == "__main__":
-    add_item()
-
-"""from sys import argv
+from sys import argv
 
 
 if __name__ == "__main__":
