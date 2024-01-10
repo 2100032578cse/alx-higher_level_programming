b5c491a4c7c5bdd96704e9feb185dfd65e9c9195
2100032578cse <uwizeyesandri@gmail.com>
2024-01-09 18:04:30 -0800

    pascal triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
-jjdj
+"""Implements pascal triangle"""
+
+
+def pascal_triangle(n):
+    """
+        returns thelist of lists
+        Args:
+            n (int): number of elements
+        Returns: the list of lists
+    """
+
+    row = [0]
+    p = [1]
+    triangle = []
+
+    if n <= 0:
+        return triangle
+    for i in range(n):
+        triangle.append(row)
+        row = (lc+rw for lc, rw i n zip(row + t, t + row))
+    return triangle
