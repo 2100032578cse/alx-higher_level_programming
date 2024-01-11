0b6da720606e07acc91d05bf3a0c0d17435cc82c
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-01-11 09:26:34 +0530

    update

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" defines obnj"""
+
+
+def is_same_class(obj, a_class):
+  """
+    checks instance of a specified class
+
+    Args:
+    obj - object
+    a_class - specified class
+
+    Returns:
+    True if object is exactly an instance of the specified class;
+    otherwise False
+    """
+    return obj.__class__ is a_class
