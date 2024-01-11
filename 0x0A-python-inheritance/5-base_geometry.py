f57ad7b690d87786aec26491f03f3f652cda7c6a
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-01-11 09:28:03 +0530

    task5

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,1 +1,15 @@
-5-base_geometry.py
+#!/usr/bin/python3
+"""
+This is module 5-base_geometry.py
+
+This module contains one class
+"""
+
+
+class BaseGeometry:
+    """
+    empty class
+
+    """
+
+    pass
