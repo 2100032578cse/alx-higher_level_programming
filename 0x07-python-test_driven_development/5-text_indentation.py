27846622dd342bf26ac42df5f09cd5bfe01b490f
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-01-11 23:25:35 +0530

    task5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,3 +5,20 @@ module for printing 2 lines
 
 
 def text_indentation(text):
+  """ text indendation comments"""
+  if type(text) is not str:
+        raise TypeError("text must be a string")
+    flag = 0
+    for a in text:
+        if flag == 0:
+            if a == ' ':
+                continue
+            else:
+                flag = 1
+        if flag == 1:
+            if a == '?' or a == '.' or a == ':':
+                print(a)
+                print()
+                flag = 0
+            else:
+                print(a, end="")
