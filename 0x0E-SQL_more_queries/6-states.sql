657c3e9a0e274a702bc004e919388213efc6ca10
2100032578cse <uwizeyesandri@gmail.com>
2024-02-15 13:00:55 -0800

    task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,5 @@
 -- script that creates the database hbtn_0d_usa
+ -- and table state
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
     PRIMARY KEY(id),
