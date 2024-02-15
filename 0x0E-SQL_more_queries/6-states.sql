8c90f8263b122c5a36abc7c37920895df4b9daa0
2100032578cse <uwizeyesandri@gmail.com>
2024-02-15 12:57:22 -0800

    added some tasks

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
--- 3-force_name
-CREATE DATABASE IF NOT EXIST hbtn_0d_usa;
+-- script that creates the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(PRIMARY KEY('id'), id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
