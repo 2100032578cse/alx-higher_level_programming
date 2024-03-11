661014e2de48fbec13eefb8525b0da80a7696b98
2100032578cse <uwizeyesandri@gmail.com>
2024-03-11 00:21:51 -0700

    fixing

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-loopp = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const loopp = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < loopp.length; i++) {
   console.log(loopp[i]);
 }
