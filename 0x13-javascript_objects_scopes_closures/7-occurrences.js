6b2aac6374f07c880cd3830ca0b6eae78cffa38d
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-03-17 19:59:57 +0530

    task7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement){
-	const outp = list.reduce(((count, temp){
-		if (temp===searchElement){
-			count = 1;
-		}else{
-			count = 0;
-		}
-	}),0);
+	const outp = list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
 	return outp;
 };
