fce6f65e575a005195a3a9e2851949aa61bce9dd
2100032578cse <uwizeyesandri@gmail.com>
2024-03-17 07:33:19 -0700

    fixed standards

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.nbOccurences = function (list, searchElement){
-	const outp = list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
-	return outp;
+exports.nbOccurences = function (list, searchElement) {
+  const outp = list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
+  return outp;
 };
