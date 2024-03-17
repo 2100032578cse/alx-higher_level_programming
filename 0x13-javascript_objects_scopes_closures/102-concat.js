3819f9f6457960be2293d88162fe7f1f7253eaf5
2100032578cse <uwizeyesandri@gmail.com>
2024-03-17 08:26:21 -0700

    conc files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-const d = fs.readFileSync(process.argv[2], 'utf8') + fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], d);
+const d = fs.readFileSync(process.argv[2], 'utf8');
+const e = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], d + e);
