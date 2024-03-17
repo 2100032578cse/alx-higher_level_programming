7a4ff9abdc4b0ea174e311e53b99f6c2681a52e0
2100032578cse <uwizeyesandri@gmail.com>
2024-03-17 06:42:06 -0700

    added

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
-module.exports = class Rectangle{
-	constructor (w, h){
-		if (w >= 1 && h >= 1){
-			this.width = w;
-			this.height = h;
-		}
-	}
-	print(){
-		const ch = 'X';
-		for(let i=0;i<this.height;i++){
-			console.log(ch.repeat(this.width));
-		}
-	}
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const ch = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(ch.repeat(this.width));
+    }
+  }
 };
