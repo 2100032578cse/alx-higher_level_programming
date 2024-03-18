1d364f769c2c4ec155ba42a28b6a635cdf7fd4bb
2100032578cse <uwizeyesandri@gmail.com>
2024-03-17 19:52:58 -0700

    fixed style

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,6 +8,6 @@ if __name__ = "__main__":
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM states ORDER BY stated.id ASC;")
-    i = .fetchall();
+    i = .fetchall()
     for i in rows:
         print(i)
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,8 @@ if __name__ = "__main__":
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("""SELECT * FROM states where name LIKE BINARY 'N%' ORDER BY states.id ASC""")
+    c.execute("""SELECT * FROM states where name
+            LIKE BINARY 'N%' ORDER BY states.id ASC""")
     i = c.fetchall()
     for i in rows:
         print(i)
