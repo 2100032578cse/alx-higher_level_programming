49aa03592ef193b38b190d3b7d122c69068cd8c4
2100032578cse <uwizeyesandri@gmail.com>
2024-03-22 04:02:40 -0700

    task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""states class"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class SomeClass(Base):
+    """ Write a python file that contains the class definition of a
+    State and an instance Base = declarative_base():
+
+    State class:
+    inherits from Base Tips
+    links to the MySQL table states
+    class attribute id that represents a column of an auto-generated,
+    unique integer, can’t be null and is a primary key
+    class attribute name that represents a column of a string with maximum 128
+    characters and can’t be null
+    You must use the module SQLAlchemy
+    Your script should connect to a MySQL server running on localhost at port 3306"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
+    name =  Column(String(128), nullable=False)
