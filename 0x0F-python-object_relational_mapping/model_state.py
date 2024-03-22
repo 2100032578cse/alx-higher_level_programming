5d1d6c54cd320d94385e160e5cf5219ea2d3686a
2100032578cse <uwizeyesandri@gmail.com>
2024-03-22 04:08:15 -0700

    added task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class SomeClass(Base):
     """ Write a python file that contains the class definition of a
     State and an instance Base = declarative_base():
@@ -18,8 +19,10 @@ class SomeClass(Base):
     class attribute name that represents a column of a string with maximum 128
     characters and can’t be null
     You must use the module SQLAlchemy
-    Your script should connect to a MySQL server running on localhost at port 3306"""
+    Your script should connect to a MySQL server running on
+    localhost at port 3306"""
 
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
-    name =  Column(String(128), nullable=False)
+    id = Column(Integer, primary_key=True,
+                nullable=False, unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
