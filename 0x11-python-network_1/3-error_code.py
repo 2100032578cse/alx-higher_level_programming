8fadcd3d502961a2d65c220edc21b622983af554
2100032578cse <uwizeyesandri@gmail.com>
2024-04-07 06:42:16 -0700

    request url display response

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL and an email, sends a POST request to
+the passed URL with the email as a parameter, and displays the body of
+the response (decoded in utf-8).
+"""
+import urllib.error
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    request = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))i
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
