a0953a6a34a21971d686ca3291bb81b8b3218c4e
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-04-07 19:52:40 +0530

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,8 +8,16 @@ import sys
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    request = requests.get(sys.argv[1])
-    if request.status_code >= 400:
-        print("Error code: {}".format(request.status_code))
-    else:
-        print(request.text)
+    var1 = sys.argv[1]
+    len1 = len(sys.argv)
+    lettr = "" if len1== 1 else var1
+    payload = {"q": lettr}
+    request = requests.post(url, data=payload)
+    try:
+        response = request.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
