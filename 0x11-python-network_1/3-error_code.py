6d0db394af17edc97a5990e7b70eb1d821dfd84d
2100032578cse <uwizeyesandri@gmail.com>
2024-04-07 06:43:21 -0700

    fix error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     request = urllib.request.Request(sys.argv[1])
     try:
         with urllib.request.urlopen(request) as response:
-            print(response.read().decode("ascii"))i
+            print(response.read().decode("ascii"))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
